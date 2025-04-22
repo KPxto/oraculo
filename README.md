7e6680a8b6f5996a3ddbadf0b14ca07abb5a246c
Kpxto <kaioopeixoto@gmail.com>
2025-04-21 22:02:40 -0300

    upload

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-## Projeto Oráculo
+# 🧙‍♂️ Oráculo - Chat com Documentos
 
-Criando aplicações usando RAG.
+O **Oráculo** é uma aplicação construída com [LangChain](https://www.langchain.com/) e [Streamlit](https://streamlit.io/), que permite que você interaja via chat com conteúdos de **documentos personalizados**. Você pode carregar arquivos como PDFs, textos, CSVs, vídeos do YouTube ou até mesmo sites, e conversar com a IA usando modelos da OpenAI ou da Groq.
+
+---
+
+## 🚀 Funcionalidades
+
+- 💬 Chat com o conteúdo de documentos
+- 📄 Suporte a múltiplos formatos: PDF, CSV, TXT, sites e vídeos do YouTube
+- 🧠 Suporte a diferentes modelos de linguagem (OpenAI e Groq)
+- 🧾 Memória de conversa preservada na sessão
+- 🖥️ Interface amigável com Streamlit
--- loaders.py
+++ loaders.py
@@ -1,35 +1,15 @@
-import os
-import streamlit as st
-from time import sleep
 from langchain_community.document_loaders import (WebBaseLoader,
                                                   YoutubeLoader,
                                                   CSVLoader,
                                                   PyPDFLoader,
                                                   TextLoader)
-# resolvendo o problema do bloqueio de alguns sites
-# q são bloqueados por segurança. Esta lib nos ajuda com a solução
-#from fake_useragent import UserAgent
 
 
 def carrega_site(url):
     docs = ''
-    # fazendo 5 tentativas para acessar o site
-    for i in range(5):
-        try:
-            # criando o user agent fake
-            #os.environ['USER_AGENT'] = UserAgent().random
-            loader = WebBaseLoader(url, raise_for_status=True)
-            lista_docs = loader.load()
-            docs = '\n\n'.join([doc.page_content for doc in lista_docs])
-            break
-        except:
-            print(f'Erro ao carregar site, tentativa: {i + 1}')
-            # esperando 2 segundos a cada tentativa
-            sleep(2)
-    if docs == '':
-        st.error('Não foi possível carregar o site')
-        st.stop()
-
+    loader = WebBaseLoader(url)
+    lista_docs = loader.load()
+    docs = '\n\n'.join([doc.page_content for doc in lista_docs])
     return docs
 
 
